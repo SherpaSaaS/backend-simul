fe28e4a4af4e91878003229224d8390abfe71b4e
zainebdr <zainebdrira99@gmail.com>
2024-09-27 18:09:58 +0100

    native lib

--- simulation_win_microservice/src/main/java/org/example/fmuWindows/services/FmuService.java
+++ simulation_win_microservice/src/main/java/org/example/fmuWindows/services/FmuService.java
@@ -123,7 +123,9 @@ public class FmuService {
                 .filter(var -> var.isTunnable() ||var.isHasConfiguration())
                 .forEach(variable -> variableDataMap.put(variable.getName(), variable));
 
-        //System.out.println("-------------- fmuVariableList ------------"+fmuVariableList);
+        System.out.println("-------------- jna devbug load  ------------"+System.setProperty("jna.debug_load", "true"));
+       // System.setProperty("jna.library.path", "path/to/your/win64/libs");
+        System.out.println("JNA Library Path: " + System.getProperty("jna.library.path"));
 
         List<String> variableNames = fmuVariableList.stream()
                 .map(variable -> variable.getName().toString())
