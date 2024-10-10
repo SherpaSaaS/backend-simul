0c1b973739566cce24ad4769e50f5b09504a017d
unknown <z.drira@sherpa-eng.com>
2024-10-10 16:45:22 +0100

    adjust sop

--- simulation_win_microservice/src/main/java/org/example/fmuWindows/services/FmuService.java
+++ simulation_win_microservice/src/main/java/org/example/fmuWindows/services/FmuService.java
@@ -161,10 +161,10 @@ public class FmuService {
 
 
         System.out.println("--------------------------- class path --------"+System.getProperty("java.class.path"));
-        NativeLibrary.addSearchPath("*.dll","/tmp/fmu_\"+\"*\"+\"/binaries/win64/\"+\"*\"+\".dll");
+        NativeLibrary.addSearchPath("*.dll","/tmp/fmu_*/binaries/win64/*.dll");
         System.out.println("-------------- new jna lib path   ------------"+System.getProperty("jna.library.path"));
         System.out.println("-------------- user dir   ------------"+System.getProperty("user.dir"));
-        System.out.println("--------------  ressource   ------------"+getClass().getResource("/resources/win32-x86-64").getPath().toString());
+        //System.out.println("--------------  ressource   ------------"+getClass().getResource("/resources/win32-x86-64").getPath().toString());
         System.out.println("--------------  jnidispatch    ------------"+System.getProperty("jnidispatch.path"));
 
         Simulation simulation = new Simulation(fmuPath);
